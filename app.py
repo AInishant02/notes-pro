727c1934be53bd5cb4dc712dde4651fffc5c2cbb
nishant <np40806@gmail.com>
2026-06-02 11:20:10 +0530

    fix: init db on gunicorn startup

--- app.py
+++ app.py
@@ -310,7 +310,8 @@ Notes:\n""" + content,
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
